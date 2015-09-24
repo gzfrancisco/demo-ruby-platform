dc96b7e57abaffcfad8eaa6a0a49beb2302ed200
zerma <zermaslan@gmail.com>
2015-09-24 17:52:05 -0500

    Created scaffold for users on MRI

--- RailsOnMri/MyApp/db/seeds.rb
+++ RailsOnMri/MyApp/db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(name: 'Carlos', age: 25, email: 'mail@mail.com')
+User.create(name: 'Alberto', age: 35, email: 'mail@mail.com')
+User.create(name: 'Paulina', age: 20, email: 'mail@mail.com')
+User.create(name: 'Cesar', age: 21, email: 'mail@mail.com')
+User.create(name: 'Diana', age: 33, email: 'mail@mail.com')
+User.create(name: 'Alba', age: 16, email: 'mail@mail.com')
+User.create(name: 'Manuel', age: 10, email: 'mail@mail.com')
+User.create(name: 'Rocío', age: 29, email: 'mail@mail.com')
+User.create(name: 'Miguel', age: 25, email: 'mail@mail.com')
+User.create(name: 'Mónica', age: 26, email: 'mail@mail.com')
